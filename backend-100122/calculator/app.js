9b4668c46b57d9bf409eaa186792faae42838f40
itai rozen <itai.rozen@mail.huji.ac.il>
2022-01-10 14:09:49 +0200

    using yargs & uniqid to perform CRUD on users

---
+++ backend-100122/calculator/app.js
@@ -0,0 +1,74 @@
+const yargs = require('yargs')(process.argv.slice(2))
+
+
+
+
+yargs.command({
+    command: 'calc',
+    describe: 'sums up two numbers',
+    builder: {
+        add: {
+            describe: 'result',
+            demandOption: true,
+            type: 'number'
+        },
+    },
+    handler: function () {
+        const num1 = yargs.argv.add
+        const num2 = yargs.argv._[1]  
+        console.log(`${num1} + ${num2} = `,num1+num2)
+    }
+})
+
+yargs.command({
+    command: 'calc',
+    describe: 'sums up two numbers',
+    builder: {
+        sub: {
+            describe: 'result',
+            demandOption: true,
+            type: 'number'
+        },
+    },
+    handler: function () {
+        const num1 = yargs.argv.sub
+        const num2 = yargs.argv._[1]  
+        console.log(`${num1} - ${num2} = `,num1-num2)
+    }
+})
+
+yargs.command({
+    command: 'calc',
+    describe: 'sums up two numbers',
+    builder: {
+        mult: {
+            describe: 'result',
+            demandOption: true,
+            type: 'number'
+        },
+    },
+    handler: function () {
+        const num1 = yargs.argv.mult
+        const num2 = yargs.argv._[1]  
+        console.log(`${num1} * ${num2} = `,num1*num2)
+    }
+})
+
+yargs.command({
+    command: 'calc',
+    describe: 'sums up two numbers',
+    builder: {
+        pow: {
+            describe: 'result',
+            demandOption: true,
+            type: 'number'
+        },
+    },
+    handler: function () {
+        const num1 = yargs.argv.pow 
+        console.log(`${num1} powered = `,num1*num1)
+    }
+})
+yargs.parse()
+
+// console.log(yargs.argv)
---
+++ backend-100122/users/app.js
@@ -0,0 +1,152 @@
+const fs = require('fs')
+const yargs = require('yargs')
+const uniqid = require('uniqid'); 
+const { argv } = require('process');
+
+const loadData = () => {
+    try {
+        const usersData =  fs.readFileSync('users.json').toString()
+        return JSON.parse(usersData)
+    } catch(e) {
+        return []
+    }
+}
+
+const saveToFile = arr => {
+    fs.writeFileSync('users.json', JSON.stringify(arr))
+}
+
+const getUser = id => {
+    const users = loadData()
+    const foundUser = users.find(user => user.id === id)
+    return foundUser
+}
+
+const getIdx = id => {
+    const users = loadData()
+    const userIdx = users.findIndex(user => user.id === id)
+    return (userIdx !== -1) ? userIdx : undefined
+}
+
+yargs.command({
+    command: 'add',
+    describe: 'add user',
+    builder: {
+        username : {
+            describe: 'username',
+            demandOption: true,
+            type: 'string'
+        },
+        email : {
+            describe: 'user email',
+            demandOption: true,
+            type: 'string'
+        },
+        password: {
+            descrive: 'user password',
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler: function () {
+        const usersArr = loadData()
+        const { username, email, password } = yargs.argv
+        const userObject = {
+            id: uniqid(),
+            name: username,
+            email: email,
+            password: password
+        }
+        usersArr.push(userObject)
+        saveToFile(usersArr)
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'read a specific user by id',
+    builder: {
+        id : {
+            describe: 'user id',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function () { 
+        const { id } = yargs.argv
+        const user = getUser(id)
+        console.log('user:',user)
+    }
+
+})
+
+yargs.command({
+    command: 'update',
+    describe: 'update a specific user by id',
+    builder: {
+        id : {
+            describe: 'user id',
+            demandOption: true,
+            type: 'string'
+        },
+        newName: {
+            describe: 'new username',
+            type: 'string'
+        },
+        newEmail: {
+            describe: 'new email',
+            type: 'string'
+        },
+        newPassword: {
+            describe: 'new password',
+            type:'string'
+        }
+    },
+    handler: function () { 
+        const { id } = yargs.argv
+        const users = loadData()
+        const user = getUser(id)
+        const userIdx = getIdx(id)
+        try {
+            user.name = yargs.argv.newName || user.name 
+            user.email = yargs.argv.newEmail || user.email 
+            user.password = yargs.argv.newPassword || user.password
+            console.log('new name: ',yargs.argv.newName)
+            console.log('user: ',user)
+            users.splice(userIdx,1,user)
+            saveToFile(users)
+        } catch(e) {
+            console.log('user not found');
+        }
+    }
+})
+
+yargs.command({
+    command: 'delete',
+    describe: 'delete a user',
+    builder: {
+        id: {
+            describe: 'user id',
+            demandOption:true,
+            type: 'string'
+        }
+    },
+    handler: () => {
+        const { id } = yargs.argv
+        const users = loadData()
+        const user = getUser(id)
+        const userIdx = getIdx(id)
+        try {
+            user.name = yargs.argv.newName || user.name 
+            user.email = yargs.argv.newEmail || user.email 
+            console.log('new name: ',yargs.argv.newName)
+            console.log('user: ',user)
+            users.splice(userIdx,1)
+            saveToFile(users)
+        } catch(e) {
+            console.log('user not found');
+        }
+    }
+})
+
+yargs.parse()
